aacda22172ad81c671a52a875087d6066f1fc05e
barcek2281 <sabdpp17@gmail.com>
2026-05-09 19:09:01 +0500

    add speed range

--- src/static/script/prepared.js
+++ src/static/script/prepared.js
@@ -2,6 +2,8 @@ import { playNote } from "./piano.js";
 const inputFile = document.getElementById("input-file");
 const playBtn = document.getElementById("play");
 const stopBtn = document.getElementById("stop");
+const speedRng = document.getElementById("speed");
+const speedLbl = document.querySelector("label");
 
 const timeouts = [];
 let userFile = null;
@@ -11,12 +13,13 @@ inputFile.addEventListener("change", (event) => {
   userFile = inputFile.files[0];
   playBtn.classList.add("active");
 });
-
+// start mucis
 playBtn.addEventListener("click", async ()=> {
     if (!userFile) return;
     const content = await userFile.text();
     const jsonContent = JSON.parse(content);
-    playMusic(jsonContent.notes);
+    const speed = speedRng.value;
+    playMusic(jsonContent.notes, speed);
     stopBtn.classList.add("active");
     playBtn.classList.remove("active");
 });
@@ -24,23 +27,39 @@ playBtn.addEventListener("click", async ()=> {
 stopBtn.addEventListener("click", ()=> {
     if (!userFile) return;
     stopBtn.classList.remove("active");
-    for(let timeout of timeouts) {
-        clearTimeout(timeout);
-    }
+    stopMusic(timeouts);
     if (!playBtn.classList.contains("active")) {
         playBtn.classList.add("active");
     }
+});
+
+speedRng.addEventListener("input", async function(){
+    speedLbl.textContent = `${this.value}x`;
+      if (!userFile) return;
+      if (!stopBtn.classList.contains("active")) {
+return;
+      }
+    const content = await userFile.text();
+    const jsonContent = JSON.parse(content);
+    stopMusic(timeouts);
+    playMusic(jsonContent.notes, this.value);
 });
 
 /**
  * 
  * @param {Object[]} notes 
  */
-function playMusic(notes) {
+function playMusic(notes, speed=1) {
     for(let note of notes) {
         const timeout = setTimeout(() => {
             playNote(note.note, true);
-        }, note.startTime);
+        }, note.startTime / speed);
         timeouts.push(timeout);
+    }
+}
+
+function stopMusic(timeoutIds) {
+    for(let timeout of timeoutIds) {
+        clearTimeout(timeout);
     }
 }
